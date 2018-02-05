3d57a2d10234003383a205822dfbd9c9391fc64c
mjavidin <majid.javidi@gmail.com>
2018-02-05 11:55:52 -0500

    Man

--- PrismDemo/ViewModels/ViewBViewModel.cs
+++ PrismDemo/ViewModels/ViewBViewModel.cs
@@ -6,7 +6,7 @@ namespace PrismDemo.ViewModels
 {
     public class ViewBViewModel : BindableBase
     {
-        private string _message = "Glareen1";
+        private string _message = "Glareen2";
         public string Message
         {
             get { return _message; }
